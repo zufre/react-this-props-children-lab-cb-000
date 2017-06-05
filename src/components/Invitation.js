1877ad54339f4b6679e8a5c7b76fdb031d0e4a3a
Luke Ghenco <lukeghenco@gmail.com>
2017-06-05 11:06:48 -0400

    Add passing solution code

--- src/components/Invitation.js
+++ src/components/Invitation.js
@@ -1,1 +1,15 @@
-// Code Invitation Component Here
+import React from 'react';
+
+class Invitation extends React.Component {
+
+  render() {
+    return (
+      <div>
+        <h1>You've been invited!</h1>
+        {this.props.children}
+      </div>
+    )
+  }
+}
+
+export default Invitation;
--- src/components/ThemedDecorations.js
+++ src/components/ThemedDecorations.js
@@ -1,1 +1,18 @@
-// Code ThemedDecoration Component Here
+import React from 'react';
+
+class ThemedDecorations extends React.Component {
+
+  render() {
+    const childrenWithExtraProp = React.Children.map(this.props.children, child => React.cloneElement(child, {
+      className: this.props.theme,
+    }));
+
+    return (
+      <div>
+        {childrenWithExtraProp}
+      </div>
+    );
+  }
+}
+
+export default ThemedDecorations;
